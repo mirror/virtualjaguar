99461abfcbc562e740a5e0900167ece9a4a5a035
Neils Wagenaar <sdlemu@ngemu.com>
2003-08-16 17:27:28 +0000

    Skeleton for the upcoming VJ GUI

---
+++ src/gui.cpp
@@ -0,0 +1,23 @@
+//
+// GUI.CPP
+//
+// Graphical User Interface support
+// by James L. Hammons
+//
+
+#include "types.h"
+#include "tom.h"
+#include "gui.h"
+
+void InitGUI(void)
+{
+}
+
+void GUIDone(void)
+{
+}
+
+void DrawText(uint16 * screen, uint32 x, uint32 y, const char * string)
+{
+	uint32 pitch = TOMGetSDLScreenPitch();
+}
---
+++ src/include/gui.h
@@ -0,0 +1,12 @@
+//
+// GUI.H
+//
+// Graphical User Interface support
+//
+
+#ifndef __GUI_H__
+#define __GUI_H__
+
+
+
+#endif	// __GUI_H__
