6aa04f4c35e217f50ffb0c4bb11af9f3a0db8a31
Shamus Hammons <jlhamm@acm.org>
2004-03-20 19:16:37 +0000

    Removed cdrom_update

--- src/include/cdrom.h
+++ src/include/cdrom.h
@@ -10,11 +10,11 @@
 void cdrom_init(void);
 void cdrom_reset(void);
 void cdrom_done(void);
-void cdrom_update(void);
+//void cdrom_update(void);
 
 uint8 CDROMReadByte(uint32 offset, uint32 who = UNKNOWN);
 uint16 CDROMReadWord(uint32 offset, uint32 who = UNKNOWN);
 void CDROMWriteByte(uint32 offset, uint8 data, uint32 who = UNKNOWN);
 void CDROMWriteWord(uint32 offset, uint16 data, uint32 who = UNKNOWN);
 
-#endif
+#endif	// __CDROM_H__
