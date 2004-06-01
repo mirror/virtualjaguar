cea8eeb20c7c5dff58856d303cd3ce229bff9224
Shamus Hammons <jlhamm@acm.org>
2004-06-01 19:15:11 +0000

    Added functions

--- src/include/cdrom.h
+++ src/include/cdrom.h
@@ -7,14 +7,19 @@
 
 #include "jaguar.h"
 
-void cdrom_init(void);
-void cdrom_reset(void);
-void cdrom_done(void);
-//void cdrom_update(void);
+void CDROMInit(void);
+void CDROMReset(void);
+void CDROMDone(void);
+
+void BUTCHExec(uint32 cycles);
 
 uint8 CDROMReadByte(uint32 offset, uint32 who = UNKNOWN);
 uint16 CDROMReadWord(uint32 offset, uint32 who = UNKNOWN);
 void CDROMWriteByte(uint32 offset, uint8 data, uint32 who = UNKNOWN);
 void CDROMWriteWord(uint32 offset, uint16 data, uint32 who = UNKNOWN);
+
+bool ButchIsReadyToSend(void);
+uint16 GetWordFromButchSSI(uint32 offset, uint32 who = UNKNOWN);
+void SetSSIWordsXmittedFromButch(void);
 
 #endif	// __CDROM_H__
