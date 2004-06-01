99606fecf072b71d3aa79164ac4f74b660e40a1b
Shamus Hammons <jlhamm@acm.org>
2004-06-01 19:14:01 +0000

    Fixed Read/Write headers, added global

--- src/include/dac.h
+++ src/include/dac.h
@@ -13,9 +13,13 @@ void DACDone(void);
 
 // DAC memory access
 
-void DACWriteByte(uint32 offset, uint8 data);
-void DACWriteWord(uint32 offset, uint16 data);
-uint8 DACReadByte(uint32 offset);
-uint16 DACReadWord(uint32 offset);
+void DACWriteByte(uint32 offset, uint8 data, uint32 who = UNKNOWN);
+void DACWriteWord(uint32 offset, uint16 data, uint32 who = UNKNOWN);
+uint8 DACReadByte(uint32 offset, uint32 who = UNKNOWN);
+uint16 DACReadWord(uint32 offset, uint32 who = UNKNOWN);
+
+// Global variables
+
+extern uint16 lrxd, rrxd;							// I2S ports (into Jaguar)
 
 #endif	// __DAC_H__
